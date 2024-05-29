d0ae5fae1a6d4b1c7c7577efc243b6c932f9c40c
Thejas <teju270934@gmail.com>
2024-05-30 00:11:57 +0530

    started the course where learnt how to read and write the file in synchrouns and asynchrouns way

---
+++ Node-farm/index.js
@@ -0,0 +1,28 @@
+const fs = require("fs"); // module to read and write the files
+
+// // blocking ,  READING AND WRITING THE FILES IN SYNCHROUNS WAY
+// const textIn = fs.readFileSync("./txt/input.txt", "UTF-8");
+// console.log(textIn);
+
+// const textOut = `This is what we all know about avocado :${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
+
+// non-blocking, READING AND WRITING THE FILES IN ASYNCHROUNS WAY
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR 💥");
+  console.log(data1);
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    if (err) return console.log("ERROR 💥");
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      if (err) return console.log("ERROR 💥");
+      console.log(data3);
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("your file has been written");
+      });
+    });
+  });
+});
+
+console.log("will read file!"); // this gets consoled first
