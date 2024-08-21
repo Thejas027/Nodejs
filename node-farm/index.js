3bb3b71f5ceca279bcd36316d508c9da62980085
Thejas <teju270934@gmail.com>
2024-08-21 19:08:50 +0530

    dynamically rendered the overview html page and through that dataobj its travesred over to get the details and replaced and joined them to get a single string and that is made to render in the localhost server:

--- node-farm/index.js
+++ node-farm/index.js
@@ -42,19 +42,66 @@ const url = require("url");
 /////////////////
 // SERVER
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-cart.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
-  if (pathName === "/" || pathName === "/overview")
-    res.end("It's a overview route name ");
-  else if (pathName === "/product") res.end("It's product route name");
+  // OVERVIEW PAGE
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, { "context-type": "text/html" });
+
+    const cardHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace("{%PRODUCT_CART%}", cardHtml);
+
+    res.end(output);
+  }
+
+  // PRODUCT PAGE
+  else if (pathName === "/product") {
+    res.writeHead(200, { "context-type": "text/html" });
+    res.end(tempProduct);
+  }
+  //API
   else if (pathName === "/api") {
     res.writeHead(200, { "context-type": "application/json" });
     res.end(data);
-  } else {
+  }
+  //Page not found
+  else {
     res.writeHead(404, {
       "context-type": "text/html",
     });
