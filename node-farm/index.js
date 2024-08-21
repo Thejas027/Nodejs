b0624645d304af27cc251eaf8231a14121e98b6e
Thejas <teju270934@gmail.com>
2024-08-21 20:13:35 +0530

    created a  new folder called modules and the function moved there to export it and made it use in the index.js file

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,25 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-const path = require("path");
 
-// // replace function
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-};
+const replaceTemplate = require("./modules/replaceTemplate");
 
 // // Template calls
 const tempOverview = fs.readFileSync(
