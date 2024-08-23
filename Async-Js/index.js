96edf1e7057168c4705b154e2999ed82a1cd041b
Thejas <teju270934@gmail.com>
2024-08-23 13:20:17 +0530

    the then and promise code is written using async function with error handling same functionality but differnent way of writting

--- Async-Js/index.js
+++ Async-Js/index.js
@@ -19,6 +19,43 @@ const writeFilePro = (file, data) => {
   });
 };
 
+// 1.call back hell
+/*
+fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
+  if (error) {
+    console.error("Error reading the file:", error);
+    return;
+  }
+
+  const breed = data.toString().trim(); // Convert buffer to string and trim any whitespace
+  console.log(`Breed: ${breed}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .end((err, res) => {
+      if (err) {
+        console.error("Error fetching dog image:", err);
+        return;
+      }
+
+      const dogImage = res.body.message;
+      console.log(dogImage);
+
+      fs.writeFile("dog-image.txt", dogImage, (err) => {
+        if (err) {
+          console.error("Error writing to file:", err);
+        } else {
+          console.log("Random dog image saved to file");
+        }
+      });
+    });
+});
+
+*/
+
+// 2. using then and user created promises
+
+/*
 readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
     console.log(`Breed : ${data}`);
@@ -35,3 +72,24 @@ readFilePro(`${__dirname}/dog.txt`)
   .catch((err) => {
     console.log(err.message);
   });
+*/
+
+// 3. using async function
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed : ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePro("dog-image.txt", res.body.message);
+    console.log("Random dog imagae saved to file");
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+getDogPic();
