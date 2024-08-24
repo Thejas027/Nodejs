f09866c16f017015c312db4e1a12cf539d71caf9
Thejas <teju270934@gmail.com>
2024-08-24 18:29:07 +0530

    pushed all other folder to learning and updated the sever.js and packages folders

--- Natours/server.js
+++ Natours/server.js
@@ -1,7 +1,33 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB)
+  .then(() => console.log('DB connection is successful'))
+  .catch((err) => {
+    console.log('DB connection error:', err);
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
