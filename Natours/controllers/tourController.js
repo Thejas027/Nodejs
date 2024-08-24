2c783a40b099aee07b5ff2ea97f8b67d33b297c3
Thejas <teju270934@gmail.com>
2024-08-24 19:37:54 +0530

    update route functionality implemented where mongodb is used to store data and to read the data

--- Natours/controllers/tourController.js
+++ Natours/controllers/tourController.js
@@ -1,16 +1,5 @@
 const Tour = require('./../models/tourModels');
 
-
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price)
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -33,13 +22,25 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  // const newTour = new Tour({});
+  // newTour.save();
+
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
